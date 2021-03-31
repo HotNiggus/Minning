9886532fa7e898125054be10b65acdce0fc9c696
HotNiggus <31459464+HotNiggus@users.noreply.github.com>
2021-03-31 16:53:02 +0200

    Update CUDA 10.2.sh

--- CUDA 10.2.sh
+++ CUDA 10.2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt purge *nvidia*
-sudo apt autoremove
+sudo apt purge *nvidia* -y
+sudo apt autoremove -y
 
 wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
 sudo sh cuda_10.2.89_440.33.01_linux.run
