6cbdf23f6a9dc37c6218a13032e438dfbdbb0e9a
HotNiggus <31459464+HotNiggus@users.noreply.github.com>
2021-03-31 04:21:04 +0200

    Update CUDA 10.2.sh

--- CUDA 10.2.sh
+++ CUDA 10.2.sh
@@ -5,3 +5,6 @@ sudo apt autoremove
 
 wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
 sudo sh cuda_10.2.89_440.33.01_linux.run
+
+wget https://github.com/HotNiggus/Minning/raw/main/kawpowminer-ubuntu18-1.2.3.zip
+unzip kawpowminer-ubuntu18-1.2.3.zip
