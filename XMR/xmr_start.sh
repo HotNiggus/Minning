52fcc711db68c2fbd909028e1bd1af111441f3e8
HotNiggus <31459464+HotNiggus@users.noreply.github.com>
2021-04-04 17:54:19 +0200

    Update xmr_start.sh

--- XMR/xmr_start.sh
+++ XMR/xmr_start.sh
@@ -4,7 +4,7 @@ wget https://github.com/fireice-uk/xmr-stak/releases/download/1.0.5-rx/xmr-stak-
 
 tar -xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 
-!%cd xmr-stak-rx-linux-1.0.5-cpu
+%cd xmr-stak-rx-linux-1.0.5-cpu
 
 wget https://raw.githubusercontent.com/HotNiggus/Minning/main/XMR/config.txt
 wget https://raw.githubusercontent.com/HotNiggus/Minning/main/XMR/cpu.txt
