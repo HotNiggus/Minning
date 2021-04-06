bbb3d6648ee6540dc26092b012fddf2bd518da6e
HotNiggus <31459464+HotNiggus@users.noreply.github.com>
2021-04-06 05:26:14 +0200

    Update xmr_start.sh

--- XMR/xmr_start.sh
+++ XMR/xmr_start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo apt-get update
 wget https://github.com/fireice-uk/xmr-stak/releases/download/1.0.5-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 
 tar -xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz
